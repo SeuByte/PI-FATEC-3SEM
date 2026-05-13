369c3d6971a2173220438d6b4969084def6c2ec3
MATHEUS ALEXANDRE CLEMENTE RODRIGUES <2901392513002@fatecararas.com>
2026-05-13 20:31:59 -0300

    Corrigindo o tipo de dado no models do core

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views.produtos_views import listar_produtos, listar_produto_id
 
 urlpatterns = [
- path('listar_produtos/', listar_produtos),
- path('listar_produtos/<int:id>/', listar_produto_id),
+ path('produtos/', listar_produtos),
+ path('produtos/<int:id>/', listar_produto_id),
 ]
