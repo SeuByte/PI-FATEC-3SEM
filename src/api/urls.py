2a35f2fbfe8ed4b61bb18f32cb4963d06dfc0a4a
PEDRO BENEVIDES SOUZA <2901392513023@fatecararas.com>
2026-06-09 21:08:45 -0300

    Arrumando as URLS

--- src/api/urls.py
+++ src/api/urls.py
@@ -6,16 +6,24 @@ from src.api.views.listar_senha_views import ListarSenhaViews
 from src.api.views.funcionario_views import CadastroFuncionarioView
 
 urlpatterns = [
- path('produtos/', listar_produtos),#Funcionando
- path('produtos/<str:id>/', listar_produto_id),#Funcionando
- path('deletar_produto/<str:id>/', deletar_produto), #Funcionando
- path('editar_produto/<str:id>/', editar_produto),#Funcionando
- path('criar_produto/', criar_produto),#Funcionando
- path('cadastro_api/', RegistroView.as_view(), name='api_cadastro'),
- path('listar_cliente/', ListarCliente.as_view(), name='api-listar_clientes'),
- path('login_cliente/', LoginCliente.as_view(), name= 'api_Login_Cliente'),
- path('resetar_senha_cliente/', ResetarSenha.as_view(), name='api_Resetar_senha_cliente'),
- path('recuperar_senha/', ListarSenhaViews.as_view()),
- path('confirmar-recuperacao/', ConfirmarRecuperacaoView.as_view()),
- path('cadastro_funcionario/', CadastroFuncionarioView.as_view(), name='api_cadastro_funcionario')
+    # Produtos
+ path('produtos/', listar_produtos, name='listar_produtos'),#Funcionando
+    path('produtos/<str:id>/', listar_produto_id, name='listar_produto_id'),#Funcionando
+    path('deletar_produto/<str:id>/', deletar_produto, name='deletar_produto'),#Funcionando
+    path('editar_produto/<str:id>/', editar_produto, name='editar_produto'),#Funcionando
+    path('criar_produto/', criar_produto, name='criar_produto'),#Funcionando
+
+    # Clientes
+    path('cadastrar_cliente/', cadastrar_cliente, name='cadastrar_cliente'),#Funcionando
+    path('editar_cliente/<str:cliente_id>/', editar_cliente, name='editar-cliente'),#Funcionando
+    path('deletar_cliente/<str:cliente_id>/', deletar_cliente, name='deletar_cliente'),
+    path('listar_cliente/', listar_clientes, name='listar_clientes'),#Funcionando
+    path('login_cliente/', login_cliente, name='login_cliente'),#Funcionando
+    path('rota-de-teste/', pagina_protegida, name='rota-de-teste'),
+
+    #Funcionarios e Recuperação de Senha
+    path('resetar_senha_cliente/', ResetarSenha.as_view(), name='api_Resetar_senha_cliente'),
+    path('recuperar_senha/', ListarSenhaViews.as_view()),
+    path('confirmar-recuperacao/', ConfirmarRecuperacaoView.as_view()),
+    path('cadastro_funcionario/', CadastroFuncionarioView.as_view(), name='api_cadastro_funcionario')
 ]
